4e36f788e198bcb504b2705b444a20182e149f7c
Rickard Englund <rickard.englund@gmail.com>
2018-03-22 18:57:08 +0100

    test

--- src/test/java/se/cygni/palmithor/tdd/CalculatorTest.java
+++ src/test/java/se/cygni/palmithor/tdd/CalculatorTest.java
@@ -13,8 +13,17 @@ public class CalculatorTest {
 
     @Test
     public void test() {
-        assertThat(calculator.sumAll().get()).isEqualTo(1); // fail un purpose
+        assertThat(calculator.sumAll().get()).isEqualTo(0);
     }
 
+    @Test
+    public void test2() {
+        assertThat(calculator.sumAll(null)).isEqualTo(Optional.empty());
+    }
+
+    @Test
+    public void test3() {
+        assertThat(calculator.sumAll(1,2,3)).get().isEqualTo(6);
+    }
 
 }
